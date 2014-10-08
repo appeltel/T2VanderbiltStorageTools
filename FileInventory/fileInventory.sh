5abd4953fd227e8f84bf620627cc2625d8729ec8
Eric <eric.appelt@gmail.com>
2014-10-07 19:33:57 -0500

    Switched example crontab location and added missing python command to the fileInventory script

--- FileInventory/fileInventory.sh
+++ FileInventory/fileInventory.sh
@@ -5,3 +5,5 @@ eval `/usr/local/bin/config_pkg -sh -a lio`
 cd $HOME/T2VanderbiltStorageTools/FileInventory
 /usr/local/lio/bin/lio_du -h -s @:/cms/store/ > $HOME/T2VanderbiltStorageTools/FileInventory/store.inv
 /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > $HOME/T2VanderbiltStorageTools/FileInventory/store.user.inv
+
+/usr/local/bin/python reportFileInventory.py
