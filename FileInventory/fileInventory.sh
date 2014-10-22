26963aae6cb97bf8e556c8a0a5f41441c53f3d3a
Eric <eric.appelt@gmail.com>
2014-10-22 14:42:04 -0500

    Add 2 retries if the lio_du exits with error or returns less than three lines of output

--- FileInventory/fileInventory.sh
+++ FileInventory/fileInventory.sh
@@ -15,10 +15,36 @@ fi
 /usr/local/lio/bin/lio_du -h -s @:/cms/store/ > $FI_HOME/store.inv.$COUNTER
 STORE_RESULT=$?
 echo "$(date) - performed inventory of /cms/store/ with exit code $STORE_RESULT" >> $FI_HOME/fileInventory.log
+# retry on 2 line output or error code twice
+if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $STORE_RESULT -ne 0 ]]; then
+  sleep 300
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/ > $FI_HOME/store.inv.$COUNTER
+  STORE_RESULT=$?
+  echo "$(date) - Retry 1: /cms/store/ with exit code $STORE_RESULT" >> $FI_HOME/fileInventory.log
+fi
+if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $STORE_RESULT -ne 0 ]]; then
+  sleep 300
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/ > $FI_HOME/store.inv.$COUNTER
+  STORE_RESULT=$?
+  echo "$(date) - Retry 2: /cms/store/ with exit code $STORE_RESULT" >> $FI_HOME/fileInventory.log
+fi
 
 /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > $FI_HOME/store.user.inv.$COUNTER
 USER_RESULT=$?
-echo "$(date) - performed inventory of /cms/store/user/ with exit code $STORE_RESULT" >> $FI_HOME/fileInventory.log
+echo "$(date) - performed inventory of /cms/store/user/ with exit code $USER_RESULT" >> $FI_HOME/fileInventory.log
+# retry on 2 line output or error code twice
+if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $USER_RESULT -ne 0 ]]; then
+  sleep 300
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > $FI_HOME/store.user.inv.$COUNTER
+  USER_RESULT=$?
+  echo "$(date) - Retry 1: /cms/store/user/ with exit code $USER_RESULT" >> $FI_HOME/fileInventory.log
+fi
+if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $USER_RESULT -ne 0 ]]; then
+  sleep 300
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > $FI_HOME/store.user.inv.$COUNTER
+  USER_RESULT=$?
+  echo "$(date) - Retry 2: /cms/store/user/ with exit code $USER_RESULT" >> $FI_HOME/fileInventory.log
+fi
 
 # Report to webpage
 /usr/local/bin/python reportFileInventory.py $COUNTER
