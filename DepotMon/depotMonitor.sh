d7e45bd88455f9cf7766c8c062445e55f1eadbdf
Eric <eric.appelt@gmail.com>
2015-05-29 15:00:54 -0500

    move all directory specific stuff into the crontab entry

--- DepotMon/depotMonitor.sh
+++ DepotMon/depotMonitor.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 PATH="/usr/local/bin:$PATH"
 eval `/usr/local/bin/config_pkg -sh -a lio`
-cd /home/appelte1/T2VanderbiltStorageTools/DepotMon
 /usr/local/bin/python reportDepotStatus.py $1
