dcd044d8bde2ca86574d03b3ce45b3ae85bfe923
Eric <eric.appelt@gmail.com>
2015-05-31 15:13:52 -0500

    workaround for possible lio_du bug

--- FileInventory/fileInventory.sh
+++ FileInventory/fileInventory.sh
@@ -27,19 +27,23 @@ if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $STORE_RESULT -ne 0 ]]; then
   echo "$(date) - Retry 2: /cms/store/ with exit code $STORE_RESULT" >> fileInventory.log
 fi
 
-/usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > store.user.inv.$COUNTER
+# Seems to be some lio_du bug right now (5/31/2015) that 
+# makes /cms/store/user/ report nothing, but /cms/store/user/*
+# works.
+
+/usr/local/lio/bin/lio_du -h -s @:/cms/store/user/* > store.user.inv.$COUNTER
 USER_RESULT=$?
 echo "$(date) - performed inventory of /cms/store/user/ with exit code $USER_RESULT" >> fileInventory.log
 # retry on 2 line output or error code twice
 if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $USER_RESULT -ne 0 ]]; then
   sleep 300
-  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > store.user.inv.$COUNTER
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/* > store.user.inv.$COUNTER
   USER_RESULT=$?
   echo "$(date) - Retry 1: /cms/store/user/ with exit code $USER_RESULT" >> fileInventory.log
 fi
 if [[ $( wc -l < store.inv.$COUNTER ) -lt 3 || $USER_RESULT -ne 0 ]]; then
   sleep 300
-  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/ > store.user.inv.$COUNTER
+  /usr/local/lio/bin/lio_du -h -s @:/cms/store/user/* > store.user.inv.$COUNTER
   USER_RESULT=$?
   echo "$(date) - Retry 2: /cms/store/user/ with exit code $USER_RESULT" >> fileInventory.log
 fi
